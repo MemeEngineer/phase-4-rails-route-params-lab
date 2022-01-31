2e2b93084f49064e8ed373f36b1413b70cfd7924
Johnny Wu <johnnyw131@gmail.com>
2022-01-31 02:09:25 -0500

    commit tree 9866f98d5de2ac88468cd03366b22fad2c44f28b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    #bonus
+
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
+    
+    
+    # students = Student.all
+    # render json: students
   end
 
+  def id
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
